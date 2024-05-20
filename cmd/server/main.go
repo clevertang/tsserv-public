560890b14d259c8eac92e057427fcdef45df3167
tang.xin <tang.xin@woven-planet.global>
2024-05-20 16:04:48 +0900

    update main add signal chanel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
-	"log"
-
+	"github.com/tinkermode/tsserv/pkg/logger"
 	"github.com/tinkermode/tsserv/pkg/tsserv"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	var port int
-	const defaultPort = 8080
-
-	flag.IntVar(&port, "p", defaultPort, "port number of server")
+	port := flag.Int("p", 8080, "port number of server")
 	flag.Parse()
 
-	log.Printf("Starting server on port %d\n", port)
+	logger.InfoLogger.Printf("Starting server on port %d\n", *port)
 
-	s := tsserv.New(port)
+	s := tsserv.New(*port)
 
-	log.Fatalf("Server exited with error %v\n", s.Run())
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		logger.InfoLogger.Println("Shutting down server...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := s.Shutdown(ctx); err != nil {
+			logger.ErrorLogger.Fatalf("Server forced to shutdown: %v", err)
+		}
+		logger.InfoLogger.Println("Server gracefully stopped")
+	}()
+
+	if err := s.Run(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		logger.ErrorLogger.Fatalf("Server exited with error: %v", err)
+	}
 }
